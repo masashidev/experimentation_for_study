fcb9b4ab0bfbbb7756501ca19f0473dd9eb1dbb3
Masashi.i <masashi.itamura.spt90@gmail.com>
2024-01-18 18:49:10 +0900

    add words model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :microposts
+  resources :words
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "microposts#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
